f6076bb2b97e9eda17bd8fb55a65fc0c487a231d
KonstantinLokotosh <Lokotoch777@mail.ru>
2019-11-07 15:45:12 +0300

    Added Normal Rule base

--- Task4/production_model.py
+++ Task4/production_model.py
@@ -1,5 +1,6 @@
 import re
 import sys
+from collections import deque
 
 def check_string(value):
     """
@@ -8,7 +9,7 @@ def check_string(value):
     :return: True - строка подходит для применения Forward и Backward,
     False - строка не подходит для применения Forward и Backward
     """
-    match = re.match("""^[а-яё ]+$""", value)
+    match = re.match("""^[а-яё \-_a-zA-Z]+$""", value)
     return bool(match)
 
 
@@ -22,6 +23,8 @@ def forward_pass(rule_base, s, final_state):
     :param s: факты
     :return: заключение
     """
+
+    rule_base = list(reversed(rule_base))
     work_base = s.split()
     added = False
     while not added:
@@ -32,20 +35,55 @@ def forward_pass(rule_base, s, final_state):
                 if x not in work_base:
                     flag = False
             if flag:
-                if rule_base[i].split()[-1] in final_state:
-                    return rule_base[i].split()[-1]
-                work_base.append(rule_base[i].split()[-1])
-                added = False
+                if rule_base[i].split() != []:
+                    if rule_base[i].split()[-1] in final_state:
+                        return rule_base[i].split()[-1]
+                    work_base.append(rule_base[i].split()[-1])
+                    added = False
     return " Nothing found "
+
+def backward_pass(rule_base, s):
+    """
+
+    :param rule_base:
+    :param s:
+    :return:
+    """
+    fact = []
+    work_base = deque()
+    work_base.append(s)
+    while work_base:
+        a = work_base.popleft()
+        found = False
+        if a in fact:
+            found = True
+            continue
+        for i in range(len(rule_base)):
+            if a == rule_base[i].split()[-1]:
+                for j in rule_base[i].split()[:-1]:
+                    if j in fact:
+                        found = True
+                    if j not in work_base:
+                        work_base.append(j)
+                        fact.append(j)
+                        found = True
+        if not found:
+            print(" Not found "+str(a))
+        print(fact)
+
+
 
 
 def main():
-    file = open("Cars.txt", "r")
+    file = open("Rule_base.txt", "r")
     rule_base = file.readlines()
     file.close()
     for x in range(len(rule_base)):
-        rule_base[x] = rule_base[x].replace(",", "").replace("->", "")
-    final_state = ["Audi", "Skoda", "Toyota", "Volkswagen", "Hyundai", "BMW", "Chery", "Honda", "Datsun", "Opel"]
+        rule_base[x] = rule_base[x].replace("-", "")
+    final_state = ["CR-V", "Accord", "NSX", "Pilot", "X250", "E63", "G500", "SLS", "Vits", "S63", "Vesta", "Нива",
+                   "Газель", "LC200", "Camry", "Hillux", "Rav4", "GT86", "Focus", "Raptor", "Transit", "Explorer", "Cuga",
+                   "Mustang", "GT40", "i40", "Solaris", "Sonata", "H-1", "X5", "X7", "M5", "Series7", "Q5", "Q7", "A7",
+                   "A8", "R8", "Cruz", "Camaro", "Capita", "NIVA", "Tahoe"]
     print(" Hello, enter qualities of the car and program will predict the brand of the car")
     print(" For exit from input \" exit \" or \" break \"")
     print(" Example:купе мощная экономная")
